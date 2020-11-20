08b4d7bfab0dd9e727c604e22d8a2f7239d05b2f
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2020-11-19 21:01:10 -0600

    Reducción de tiempo en get_top_ten

--- Model/Player.py
+++ Model/Player.py
@@ -94,8 +94,9 @@ class Player(declarative_base()):
 		DB = Player.init_connection()
 		counter: int = 0
 		players: dict = {}
-		for player in DB.query(Player.nombres, Player.puntuacion).order_by(
-				Player.puntuacion.asc()).limit(10):
+		results: list = DB.query(Player.nombres, Player.puntuacion).order_by(
+			Player.puntuacion.asc()).limit(10)
+		for player in results:
 			values: dict = {
 				"name": player.nombres,
 				"points": str(player.puntuacion)
