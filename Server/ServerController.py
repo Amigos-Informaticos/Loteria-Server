89d681d8b1c759f7a53e45920ab4eb4c12861dff
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2021-01-02 17:46:50 -0600

    Implementación de control

--- Server/ServerController.py
+++ Server/ServerController.py
@@ -1,7 +1,13 @@
+import inspect
+
 from Server.Controller.RoomController import RoomController
 
 
 class ServerController:
+	def list_methods(self) -> None:
+		for method in inspect.getmembers(self, predicate=inspect.ismethod):
+			print(method.__str__())
+
 	def view_rooms(self):
 		for room in RoomController.rooms:
 			print(room.id)
