b7e1c8d66574c8d0b48c6d6e4b01a23e19f9971c
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2021-01-02 18:13:43 -0600

    Implementación de control

--- Server/Controller/PlayerController.py
+++ Server/Controller/PlayerController.py
@@ -102,6 +102,8 @@ class PlayerController:
 				watchable_user["event"][values["event"]] = True
 				PlayerController.connected_clients.append(watchable_user)
 				response = "OK"
+			else:
+				response = "ALREADY SUBSCRIBED"
 		else:
 			response = "WRONG ARGUMENTS"
 		return response
